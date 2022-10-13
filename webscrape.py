5a15855df348d23cd6c886a2ac73fa1c9771c5b4
id-aaryan <am5020@imperial.ac.uk>
2022-10-13 12:16:50 +0100

    [am5020, ss5120, aan120] webscraped and produces nutrition json

---
+++ webscrape.py
@@ -0,0 +1,29 @@
+
+from selenium import webdriver
+import json
+from bs4 import BeautifulSoup
+
+driver = webdriver.Chrome('./chromedriver')
+
+url = "https://www.tesco.com/groceries/en-GB/products/289623609"
+driver.get(url)
+page = driver.page_source
+soup = BeautifulSoup(page, 'lxml')
+
+nutrition_table = soup.select('table')[0]
+
+rows = nutrition_table.select('tr')
+macros_value = {}
+for row in rows:
+    info = row.select('td')
+    if len(info) == 0:
+        continue
+    macro = str(info[0].findAll(text=True)[0]).strip(' ')
+    value = str(info[1].findAll(text=True)[0]).strip(' ')
+    macros_value[macro] = value
+
+nutrition_json = json.dumps(macros_value, indent=4)
+
+print(nutrition_json)
+
+driver.quit()
