86e83a7f3e80095683fe26103fc156052c653024
id-aaryan <am5020@imperial.ac.uk>
2022-10-25 18:22:59 +0100

    [am5020] refactor to include nlp pipeline from huggingface

--- webscrape.py
+++ webscrape.py
@@ -1,29 +1,74 @@
 import json
+import sys
 from util import get_soup
+from transformers import pipeline
+question_answerer = pipeline("question-answering", model='distilbert-base-cased-distilled-squad')
 
-def create_json(url):
+def create_json(url, macro):
     soup = get_soup(url)
-    macros_value = {}
-    try:
-        nutrition_table = soup.select('table')[0]
-        rows = nutrition_table.select('tr')
-        for row in rows:
-            info = row.select('td')
-            if len(info) == 0:
-                continue
-            macro = str(info[0].findAll(text=True)[0]).strip(' ')
-            value = str(info[1].findAll(text=True)[0]).strip(' ')
-            macros_value[macro] = value
-    except IndexError:
-        pass
+    # try:
+    links = soup.select('a')
+    products = set()
+    i = 0
+    for link in links:
+        to_scrape = 'https://www.tesco.com' + str(link.get('href'))
+
+        if '/products/' in to_scrape:
+            i+=1
+            products.add(to_scrape)
+            if i == 1:
+                break
+    print(products)
+    product_to_macro = {}
+    for product in products:
+        print("")
+        # result = question_answerer(question=q, context=c)
+        # print(result)
+    #     product_to_macro[product] = 0
+    #
 
     json_object = json.dumps({
-        "url": url,
-        # name, and company
-        "tag1": 'table',
-        "tag2": 'tr',
-        "tag3": 'td',
-        "health": macros_value,
+        "search_query" : url,
+        "products_to_macro": product_to_macro
     })
 
     return json_object
+
+def helper(product_url, macro):
+    gs = get_soup(product_url)
+    i = 0
+    results = []
+    for tag in gs.find_all():
+        context = str(tag)
+        if "Fat" in context:
+            print(context)
+            i += 1
+            q = "what is " + macro
+            # result = question_answerer(question=q, context=context)
+            # results.append(result)
+            if i == 2:
+                break
+
+
+
+    print(i)
+    # for tag in gs.find_all():
+    #     print(str(tag))
+    #     i += 1
+    #     if macro in tag:
+    #         l.append(str(tag))
+    #     # find the value of the macro, store it in some set()
+    #     # n = valueOfMacro(macro, soup)
+    #     q = "what is " + macro
+    # c = str(gs)
+    # print([])
+    # print(c)
+    # return c
+
+def main(url, macro):
+    print(helper("https://www.tesco.com/groceries/en-GB/products/277043162", macro))
+    # print(create_json(url, macro))
+
+if __name__ == "__main__":
+    # python3 webscrape.py "https://www.tesco.com/groceries/en-GB/shop/fresh-food/all" "fat"
+    main(sys.argv[1], sys.argv[2])
