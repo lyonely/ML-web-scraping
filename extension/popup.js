31072de4caf8d6c12fd2ee14140e9002aef7f30a
Sidharth Jain <sidharthjain20022@gmail.com>
2022-10-28 16:15:03 +0100

    [sj1720, tg1120] feat: URL of the page the user is on is extracted

--- extension/popup.js
+++ extension/popup.js
@@ -3,8 +3,13 @@ let btn = document.createElement("button");
 btn.innerHTML = "Go";
 
 btn.onclick = function () {
-    window.open('https://www.palantir.com/');
-    //alert("Button is clicked");
+    // window.open('https://www.palantir.com/');
+
+    chrome.tabs.query({active:true}, tabs=>{
+        const tab=tabs[0];
+        alert("The URL of this page is:  " + tab.url);
+      }
+    )
 
 };
 
