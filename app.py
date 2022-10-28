c088eed3e8441524c568be2298b320e871815a24
Yufeng Zhang <z.y.f.9955@gmail.com>
2022-10-28 17:54:43 +0100

    [yz10519] feat: added route for POST request to get information from ML pipeline

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+
+from pipeline import products_to_macro_json
 
 app = Flask(__name__)
 CORS(app)
@@ -8,3 +10,11 @@ CORS(app)
 def health():
     """Used to test if the app is running"""
     return "hello world"
+
+@app.route('/macro', methods=['POST', 'GET'])
+def login():
+    """Takes a POST request with 'url' and 'macro' fields and returns the relevant information"""
+    if request.method == 'POST':
+        if (request.form['url'] and request.form['macro']):
+            return products_to_macro_json(request.form['url'], request.form['macro'])
+    return "Error: received GET request instead of POST request"
