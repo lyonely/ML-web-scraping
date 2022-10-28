8e1e6cc1931f31cd98b9c740b9795cbd379a0bae
Tanish Goel <tanish@dyn3237-124.wlan.ic.ac.uk>
2022-10-28 15:47:23 +0100

    [tg1120][sj1720] fix: requests with backend work

--- extension/popup.js
+++ extension/popup.js
@@ -4,8 +4,12 @@ btn.innerHTML = "Go";
 
 btn.onclick = function () {
     window.open('https://www.palantir.com/');
-    alert("Button is clicked");
+    //alert("Button is clicked");
 
 };
+
+fetch('https://ml-web-scraping.herokuapp.com/')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 
 document.body.appendChild(btn);
