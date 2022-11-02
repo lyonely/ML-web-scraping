677bd2fce8d4ee30befbaf8ebde20a2d21736d1e
Tanish Goel <tanish@dyn3228-55.wlan.ic.ac.uk>
2022-11-02 15:00:10 +0000

    [tg1120] [sj1720] feat: send post request with required data and display response

--- extension/popup.js
+++ extension/popup.js
@@ -2,19 +2,42 @@ let btn = document.createElement("button");
 
 btn.innerHTML = "Go";
 
+const tab = "";
+
+var label;
+
+
+
 btn.onclick = function () {
-    // window.open('https://www.palantir.com/');
+
+    var form = document.getElementById("form")
+    label = document.getElementById("fname")
 
     chrome.tabs.query({active:true}, tabs=>{
-        const tab=tabs[0];
-        alert("The URL of this page is:  " + tab.url);
+        tab=tabs[0];
+        alert("The URL of this page is:  " + tab.url + ". The label is: ") + label;
       }
     )
 
 };
 
-fetch('https://ml-web-scraping.herokuapp.com/')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+fetch('http://35.198.85.60/macro', {
+  method: 'POST',
+  body: JSON.stringify({
+    url:tab.url,
+    macro:label,
+
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  }
+  })
+  .then(function(response){ 
+  return response.json()})
+  .then(function(data)
+  {console.log(data)
+  body=document.getElementById("bd")
+  body.innerHTML = data  
+}).catch(error => console.error('Error:', error)); 
 
 document.body.appendChild(btn);
