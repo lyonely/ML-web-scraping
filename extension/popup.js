e85fb3524a04a34de51e627ba7f116ec6ac6aa13
Tanish Goel <tanish@dyn3232-71.wlan.ic.ac.uk>
2022-11-16 12:34:09 +0000

    [tg1120] [am1520] [sj1720] [yz10519] fix: fixed api requests and displays in basic form

--- extension/popup.js
+++ extension/popup.js
@@ -3,27 +3,55 @@ let btn = document.createElement("button");
 btn.innerHTML = "Go";
 btn.id = "x"
 
+const textOutput = document.querySelector("#showOutput");
+
 // btn.style.background = "#3dfe3a";
 
 document.body.appendChild(btn);
-var tab = "";
 var label = "";
 
-btn.onclick = function () {
+btn.addEventListener("click", fetchHandler);
 
+// showing output
+function displayOutput() {
+  loader.classList.add("visible");
+  // to stop loading after some time
+}
+
+// hiding output 
+function hideOutput() {
+  loader.classList.remove("visible");
+}
+
+// selecting loading div
+const loader = document.querySelector("#loading");
+
+// showing loading
+function displayLoading() {
+    loader.classList.add("display");
+}
+
+// hiding loading 
+function hideLoading() {
+    loader.classList.remove("display");
+}
+
+async function fetchHandler(event) {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  console.log(tab)
+
+  displayLoading()
   var form = document.getElementById("form")
   label = document.getElementById("fname").value
-
-
-  chrome.tabs.query({active:true}, tabs=>{
-      tab = tabs[0]
-    }
-  )
 
   const data = {
     url: tab.url,
     macro: label,
   }
+
+  console.log(data.url + " " + data.macro)
 
   fetch('http://35.246.204.26:5000/macro', {
     method: 'POST',
@@ -37,7 +65,17 @@ btn.onclick = function () {
         console.log(response)
         return response.json()})
       .then(function(data){
+        hideLoading()
         console.log(data)
+        displayOutput()
+        let d = data.products_to_keyword
+        console.log(d)
+        let d1 = JSON.stringify(d)
+        var result_str = ""
+        for (var key in d){
+          result_str += String(key) + ": " +  String(d[key]) + "\n"
+        }
+        textOutput.innerText = result_str
       })
       .catch(error => console.error('Error:', error));
 
