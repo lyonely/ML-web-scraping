7fbfcec1471490690d7fcc2372d6b2eb9e6aed44
id-aaryan <am5020@imperial.ac.uk>
2022-11-29 14:22:31 +0000

    [am5020] added alternate algorithm to work on

--- backend/nlp_model.py
+++ backend/nlp_model.py
@@ -2,42 +2,43 @@ from typing import List, Dict
 from rake_nltk import Rake
 import re
 
+
 class NLPModel:
     """ NLPModel object that uses a question_answerer model """
 
     def __init__(self, question_answerer):
         """ Initializes a NLPModel object with a model input"""
-        self.raker = Rake()
+        self.rake = Rake()
         self.max_searches = 32
         self.question_answerer = question_answerer
 
-    def get_keywords(self, question: str, n: int = 1):
-        self.raker.extract_keywords_from_text(question)
-        return str(self.raker.get_ranked_phrases()[0]).split(" ")
+    def get_keywords(self, question: str, n: int):
+        self.rake.extract_keywords_from_text(question)
+        try:
+            return str(self.rake.get_ranked_phrases()[0]).split(" ")[:n]
+        except IndexError:
+            return []
 
     def product_question(self, tags: List[str], question: str):
         """ get all tags of soup """
         results: Dict[str, int] = {}
         # max_answer represents the answer with the highest confidence
-        keywords = self.get_keywords(question, 10)
-        print(keywords)
+        keywords = set(self.get_keywords(question, n=3))
         search = 1
         tags_visited = {}
         tags = [tag for tag in tags if len(tag) < 700]
-        print("Starting the algorithm")
         for tag in tags:
             if search > self.max_searches:
                 break
 
-            contain = False
-            weight = 0
+            contain = len(keywords) == 0
+            weight = 0 + int(contain)
             for word in keywords:
                 if word in tag:
                     contain = True
-                    weight += 1
+                    weight += 0.5
 
             if contain:
-
                 ignore_flag = True
                 for visited in tags_visited.keys():
                     if visited in tag and tags_visited[visited] >= 2:
@@ -58,7 +59,6 @@ class NLPModel:
                         results[result["answer"]] = result["score"] / search * weight
 
                     search += 1
-        print(results)
         answer = ""
         if results:
             answer = max(results, key=results.get)
@@ -70,5 +70,21 @@ class NLPModel:
             if m := re.findall(r"(.*)<", answer):
                 answer = m[0]
 
-        print("Here's the answer:", answer)
         return answer
+
+    def alternate_algorithm(self, soup, question):
+        found = re.findall(r">([^<]*\S)<", soup)
+        found = [f for f in found if len(f) < 500]
+        results = {}
+        for a in found:
+            result = self.question_answerer(question=question, context=a)
+
+            if result["answer"] in results:
+                results[result["answer"]] += result["score"]
+            else:
+                results[result["answer"]] = result["score"]
+        print(results)
+        answer = ""
+        if results:
+            answer = max(results, key=results.get)
+        print(answer)
