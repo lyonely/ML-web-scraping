81900ce2b6b4849250115e33405c25e9fc32086f
Nicolás Anguita <nicolasanguita@Nicolass-MBP-2.lan>
2022-11-29 23:48:26 +0000

    na920 - refactoring nlp_model.py

--- backend/nlp_model.py
+++ backend/nlp_model.py
@@ -12,6 +12,7 @@ class NLPModel:
         self.MAX_SEARCHES = 32
         self.NUM_KEYWORDS = 10
         self.MAX_TAG_LENGTH = 700
+        self.MAX_VISITS_PER_TAG = 2
     
 
     
@@ -27,41 +28,48 @@ class NLPModel:
         # max_answer represents the answer with the highest confidence
         keywords = self.get_keywords(question, self.NUM_KEYWORDS)
         print(keywords)
-        num_searches = 1
         tags_visited = {}
         tags = [tag for tag in tags if len(tag) < self.MAX_TAG_LENGTH]
         print("Starting the algorithm")
+        model_call_count = 0
         for tag in tags:
-            for _ in range(self.MAX_SEARCHES):
-            # if num_searches > self.max_searches:
-            #     break
-            
-                word_map = [word in tag for word in keywords]
-                contain = any(word_map)
-                weight = sum(word_map)
+            word_map = [word in tag for word in keywords]
+            contain = any(word_map)
+            weight = sum(word_map)
 
-                if contain:
+            if contain:
+                for num_searches in range(1, self.MAX_SEARCHES +1):
+                        ignore_flag = False
+                        
 
-                    ignore_flag = True
-                    for visited in tags_visited.keys():
-                        if visited in tag and tags_visited[visited] >= 2:
-                            ignore_flag = False
-                            break
+                        for visited in tags_visited.keys():
+                            if visited in tag and tags_visited[visited] >= self.MAX_VISITS_PER_TAG:
+                                ignore_flag = True 
+                                break
 
-                    if ignore_flag:
-                        if tag in tags_visited:
-                            tags_visited[tag] += 1
-                        else:
-                            tags_visited[tag] = 1
+                        if not ignore_flag:
+                            if tag in tags_visited:
+                                tags_visited[tag] += 1
+                            else:
+                                tags_visited[tag] = 1
 
-                        result = self.question_answerer(question=question, context=tag)
+                            #print("calling model")
+                            model_call_count += 1
+                            result = self.question_answerer(question=question, context=tag)
 
-                        if result["answer"] in results:
-                            results[result["answer"]] += result["score"] / num_searches * weight
-                        else:
-                            results[result["answer"]] = result["score"] / num_searches * weight
+                            score_to_add = result["score"] / num_searches * weight
 
-                    # num_searches += 1
+                            if result["answer"] in results:
+                                results[result["answer"]] += score_to_add
+                            else:
+                                results[result["answer"]] = score_to_add
+
+        
+
+
+
+
+        print("num times called model", model_call_count)
         print(results)
         answer = ""
         if results:
