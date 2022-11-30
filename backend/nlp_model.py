7aa3c8913d2e9e05ad4033e925e50e9a6d11cfb6
Nicolás Anguita <nicolasanguita@Nicolass-MBP-2.lan>
2022-11-30 00:01:35 +0000

    na920 - debugging nlp_model

--- backend/nlp_model.py
+++ backend/nlp_model.py
@@ -35,7 +35,7 @@ class NLPModel:
         for tag in tags:
             word_map = [word in tag for word in keywords]
             contain = any(word_map)
-            weight = sum(word_map)
+            weight = sum(word_map) * 0.5
 
             if contain:
                 for num_searches in range(1, self.MAX_SEARCHES +1):
@@ -55,6 +55,7 @@ class NLPModel:
 
                             #print("calling model")
                             model_call_count += 1
+                            print("Calling model on question: " + str(question) + ", context: ", str(tag))
                             result = self.question_answerer(question=question, context=tag)
 
                             score_to_add = result["score"] / num_searches * weight
@@ -84,3 +85,79 @@ class NLPModel:
 
         print("Here's the answer:", answer)
         return answer
+
+
+    def product_question_prime(self, tags: List[str], question: str):
+        """
+        IDEAS TO REDUCE THE NUMBER OF TIMES WE CALL THE MODEL
+
+        1. make contain more strict (maybe a certain percentage has to contain the words?)
+        2. make tags a shorter list - self.MAX_TAG_LENGTH can be shorter
+        3. shouldn't we remove all the html from visited? ie just be left with the text
+        4. make numsearches shorter
+        
+        """
+        """ get all tags of soup """
+        results: Dict[str, int] = {}
+        # max_answer represents the answer with the highest confidence
+        keywords = self.get_keywords(question, self.NUM_KEYWORDS)
+        print(keywords)
+        tags_visited = {}
+        tags = [tag for tag in tags if len(tag) < self.MAX_TAG_LENGTH]
+        print("Starting the algorithm")
+        model_call_count = 0
+        for tag in tags:
+            word_map = [word in tag for word in keywords]
+            contain = any(word_map)
+            weight = sum(word_map)
+
+            if contain:
+                for num_searches in range(1, self.MAX_SEARCHES +1):
+                        ignore_flag = False
+                        
+
+                        for visited in tags_visited.keys():
+                            if visited in tag and tags_visited[visited] >= self.MAX_VISITS_PER_TAG:
+                                ignore_flag = True 
+                                break
+
+                        if not ignore_flag:
+                            if tag in tags_visited:
+                                tags_visited[tag] += 1
+                            else:
+                                tags_visited[tag] = 1
+
+                            #print("calling model")
+                            model_call_count += 1
+                            print("Calling model on question: " + str(question) + ", context: ", str(tag))
+                            result = self.question_answerer(question=question, context=tag)
+
+                            score_to_add = result["score"] / num_searches * weight
+
+                            if result["answer"] in results:
+                                results[result["answer"]] += score_to_add
+                            else:
+                                results[result["answer"]] = score_to_add
+
+        
+
+
+
+
+        print("num times called model", model_call_count)
+        print(results)
+        answer = ""
+        if results:
+            answer = max(results, key=results.get)
+
+        if ">" in answer:
+            if m := re.findall(r">(.*)<?", answer):
+                answer = m[0]
+        if "<" in answer:
+            if m := re.findall(r"(.*)<", answer):
+                answer = m[0]
+
+        print("Here's the answer:", answer)
+        return answer
+
+
